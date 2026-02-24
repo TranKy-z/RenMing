aaa1a2a4f84af69647b0fd131ac859bbef8dd26c
RenMing <tranminhky109@gmail.com>
2026-02-24 21:48:21 +0700

    upwindow

--- RenMing/RenMing/src/Renming/ApplicationClass.cpp
+++ RenMing/RenMing/src/Renming/ApplicationClass.cpp
@@ -1,12 +1,9 @@
 #include "ApplicationClass.h"
-#include <stdio.h>
 
 namespace Renming {
 	ApplicationClass::ApplicationClass() {}
 	ApplicationClass::~ApplicationClass() {}
 	void ApplicationClass::Run() {
-		int a[1000];
-		Run();
-		a[0] = 1;
+		while (true);
 	}
 }
